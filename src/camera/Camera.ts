ea1d9558e7319113a4ab2de648fee0e21e6dfaf5
sentence <snnt@protonmail.ch>
2023-12-22 14:02:45 +0100

    feat: change camera transform unit

--- src/camera/Camera.ts
+++ src/camera/Camera.ts
@@ -9,11 +9,31 @@ import FlickingError from "../core/FlickingError";
 import Panel from "../core/panel/Panel";
 import AnchorPoint from "../core/AnchorPoint";
 import * as ERROR from "../const/error";
-import { ALIGN, CIRCULAR_FALLBACK, DIRECTION, EVENTS, ORDER } from "../const/external";
-import { checkExistence, find, getFlickingAttached, getProgress, getStyle, includes, parseAlign, toArray } from "../utils";
+import {
+  ALIGN,
+  CIRCULAR_FALLBACK,
+  DIRECTION,
+  EVENTS,
+  ORDER,
+} from "../const/external";
+import {
+  checkExistence,
+  find,
+  getFlickingAttached,
+  getProgress,
+  getStyle,
+  includes,
+  parseAlign,
+  toArray,
+} from "../utils";
 import { ValueOf } from "../type/internal";
 
-import { CameraMode, BoundCameraMode, CircularCameraMode, LinearCameraMode } from "./mode";
+import {
+  CameraMode,
+  BoundCameraMode,
+  CircularCameraMode,
+  LinearCameraMode,
+} from "./mode";
 
 export interface CameraOptions {
   align: FlickingOptions["align"];
@@ -50,28 +70,36 @@ class Camera {
    * @type {HTMLElement}
    * @readonly
    */
-  public get element() { return this._el; }
+  public get element() {
+    return this._el;
+  }
   /**
    * An array of the child elements of the camera element(`.flicking-camera`)
    * @ko 카메라 엘리먼트(`.flicking-camera`)의 자식 엘리먼트 배열
    * @type {HTMLElement[]}
    * @readonly
    */
-  public get children() { return toArray(this._el.children) as HTMLElement[]; }
+  public get children() {
+    return toArray(this._el.children) as HTMLElement[];
+  }
   /**
    * Current position of the camera
    * @ko Camera의 현재 좌표
    * @type {number}
    * @readonly
    */
-  public get position() { return this._position; }
+  public get position() {
+    return this._position;
+  }
   /**
    * Align position inside the viewport where {@link Panel}'s {@link Panel#alignPosition alignPosition} should be located at
    * @ko 패널의 정렬 기준 위치. 뷰포트 내에서 {@link Panel}의 {@link Panel#alignPosition alignPosition}이 위치해야 하는 곳입니다
    * @type {number}
    * @readonly
    */
-  public get alignPosition() { return this._alignPos; }
+  public get alignPosition() {
+    return this._alignPos;
+  }
   /**
    * Position offset, used for the {@link Flicking#renderOnlyVisible renderOnlyVisible} option
    * @ko Camera의 좌표 오프셋. {@link Flicking#renderOnlyVisible renderOnlyVisible} 옵션을 위해 사용됩니다.
@@ -79,7 +107,9 @@ class Camera {
    * @default 0
    * @readonly
    */
-  public get offset() { return this._offset - this._circularOffset; }
+  public get offset() {
+    return this._offset - this._circularOffset;
+  }
   /**
    * Whether the `circular` option is enabled.
    * The {@link Flicking#circular circular} option can't be enabled when sum of the panel sizes are too small.
@@ -89,13 +119,17 @@ class Camera {
    * @default false
    * @readonly
    */
-  public get circularEnabled() { return this._circularEnabled; }
+  public get circularEnabled() {
+    return this._circularEnabled;
+  }
   /**
    * A current camera mode
    * @type {CameraMode}
    * @readonly
    */
-  public get mode() { return this._mode; }
+  public get mode() {
+    return this._mode;
+  }
   /**
    * A range that Camera's {@link Camera#position position} can reach
    * @ko Camera의 {@link Camera#position position}이 도달 가능한 범위
@@ -104,21 +138,27 @@ class Camera {
    * @property {number} max A maximum position<ko>최대 위치</ko>
    * @readonly
    */
-  public get range() { return this._range; }
+  public get range() {
+    return this._range;
+  }
   /**
    * A difference between Camera's minimum and maximum position that can reach
    * @ko Camera가 도달 가능한 최소/최대 좌표의 차이
    * @type {number}
    * @readonly
    */
-  public get rangeDiff() { return this._range.max - this._range.min; }
+  public get rangeDiff() {
+    return this._range.max - this._range.min;
+  }
   /**
    * An array of visible panels from the current position
    * @ko 현재 보이는 패널들의 배열
    * @type {Panel[]}
    * @readonly
    */
-  public get visiblePanels() { return this._visiblePanels; }
+  public get visiblePanels() {
+    return this._visiblePanels;
+  }
   /**
    * A range of the visible area from the current position
    * @ko 현재 위치에서 보이는 범위
@@ -127,28 +167,45 @@ class Camera {
    * @property {number} min A maximum position<ko>최대 위치</ko>
    * @readonly
    */
-  public get visibleRange() { return { min: this._position - this._alignPos, max: this._position - this._alignPos + this.size }; }
+  public get visibleRange() {
+    return {
+      min: this._position - this._alignPos,
+      max: this._position - this._alignPos + this.size,
+    };
+  }
   /**
    * An array of {@link AnchorPoint}s that Camera can be stopped at
    * @ko 카메라가 도달 가능한 {@link AnchorPoint}의 목록
    * @type {AnchorPoint[]}
    * @readonly
    */
-  public get anchorPoints() { return this._anchors; }
+  public get anchorPoints() {
+    return this._anchors;
+  }
   /**
    * A current parameters of the Camera for updating {@link AxesController}
    * @ko {@link AxesController}를 업데이트하기 위한 현재 Camera 패러미터들
    * @type {ControlParams}
    * @readonly
    */
-  public get controlParams() { return { range: this._range, position: this._position, circular: this._circularEnabled }; }
+  public get controlParams() {
+    return {
+      range: this._range,
+      position: this._position,
+      circular: this._circularEnabled,
+    };
+  }
   /**
    * A Boolean value indicating whether Camera's over the minimum or maximum position reachable
    * @ko 현재 카메라가 도달 가능한 범위의 최소 혹은 최대점을 넘어섰는지를 나타냅니다
    * @type {boolean}
    * @readonly
    */
-  public get atEdge() { return this._position <= this._range.min || this._position >= this._range.max; }
+  public get atEdge() {
+    return (
+      this._position <= this._range.min || this._position >= this._range.max
+    );
+  }
   /**
    * Return the size of the viewport
    * @ko 뷰포트 크기를 반환합니다
@@ -203,7 +260,9 @@ class Camera {
         prevPosition -= rangeDiff;
       }
 
-      return nearestPanel.index - 1 + getProgress(position, prevPosition, panelPos);
+      return (
+        nearestPanel.index - 1 + getProgress(position, prevPosition, panelPos)
+      );
     } else {
       const nextPanel = nearestPanel.next();
       let nextPosition = nextPanel
@@ -225,7 +284,9 @@ class Camera {
    * @type {string}
    * @readonly
    */
-  public get panelOrder() { return this._panelOrder; }
+  public get panelOrder() {
+    return this._panelOrder;
+  }
 
   // Options Getter
   /**
@@ -233,7 +294,9 @@ class Camera {
    * @ko {@link Camera#alignPosition alignPosition}이 뷰포트 엘리먼트 내의 어디에 위치해야 하는지를 나타내는 값
    * @type {ALIGN | string | number}
    */
-  public get align() { return this._align; }
+  public get align() {
+    return this._align;
+  }
 
   // Options Setter
   public set align(val: FlickingOptions["align"]) {
@@ -241,9 +304,10 @@ class Camera {
   }
 
   /** */
-  public constructor(flicking: Flicking, {
-    align = ALIGN.CENTER
-  }: Partial<CameraOptions> = {}) {
+  public constructor(
+    flicking: Flicking,
+    { align = ALIGN.CENTER }: Partial<CameraOptions> = {}
+  ) {
     this._flicking = flicking;
     this._resetInternalValues();
 
@@ -262,7 +326,10 @@ class Camera {
   public init(): this {
     const viewportEl = this._flicking.viewport.element;
 
-    checkExistence(viewportEl.firstElementChild, "First element child of the viewport element");
+    checkExistence(
+      viewportEl.firstElementChild,
+      "First element child of the viewport element"
+    );
     this._el = viewportEl.firstElementChild as HTMLElement;
     this._checkTranslateSupport();
 
@@ -329,7 +396,7 @@ class Camera {
       return new AnchorPoint({
         index: lastAnchor.index,
         position: lastAnchor.position - rangeDiff,
-        panel: lastAnchor.panel
+        panel: lastAnchor.panel,
       });
     }
   }
@@ -354,7 +421,7 @@ class Camera {
       return new AnchorPoint({
         index: firstAnchor.index,
         position: firstAnchor.position + rangeDiff,
-        panel: firstAnchor.panel
+        panel: firstAnchor.panel,
       });
     }
   }
@@ -370,7 +437,9 @@ class Camera {
   public getProgressInPanel(panel: Panel) {
     const panelRange = panel.range;
 
-    return (this._position - panelRange.min) / (panelRange.max - panelRange.min);
+    return (
+      (this._position - panelRange.min) / (panelRange.max - panelRange.min)
+    );
   }
 
   /**
@@ -406,7 +475,7 @@ class Camera {
     const flicking = getFlickingAttached(this._flicking);
     const activeIndex = flicking.control.activeIndex;
 
-    return find(this._anchors, anchor => anchor.panel.index === activeIndex);
+    return find(this._anchors, (anchor) => anchor.panel.index === activeIndex);
   }
 
   /**
@@ -461,7 +530,7 @@ class Camera {
     this._updateMode();
     this._range = this._mode.getRange();
 
-    panels.forEach(panel => panel.updateCircularToggleDirection());
+    panels.forEach((panel) => panel.updateCircularToggleDirection());
 
     return this;
   }
@@ -475,9 +544,10 @@ class Camera {
   public updateAlignPos(): this {
     const align = this._align;
 
-    const alignVal = typeof align === "object"
-      ? (align as { camera: string | number }).camera
-      : align;
+    const alignVal =
+      typeof align === "object"
+        ? (align as { camera: string | number }).camera
+        : align;
 
     this._alignPos = parseAlign(alignVal, this.size);
 
@@ -515,7 +585,7 @@ class Camera {
     if (!flicking.horizontal || !flicking.adaptive || !activePanel) return;
 
     flicking.viewport.setSize({
-      height: activePanel.height
+      height: activePanel.height,
     });
   }
 
@@ -528,10 +598,10 @@ class Camera {
   public updateOffset(): this {
     const flicking = getFlickingAttached(this._flicking);
     const position = this._position;
-    const unRenderedPanels = flicking.panels.filter(panel => !panel.rendered);
+    const unRenderedPanels = flicking.panels.filter((panel) => !panel.rendered);
 
     this._offset = unRenderedPanels
-      .filter(panel => panel.position + panel.offset < position)
+      .filter((panel) => panel.position + panel.offset < position)
       .reduce((offset, panel) => offset + panel.sizeIncludingMargin, 0);
 
     this._circularOffset = this._mode.getCircularOffset();
@@ -586,10 +656,16 @@ class Camera {
 
     if (renderer.rendering || !flicking.initialized) return this;
 
-    const actualPosition = this._position - this._alignPos - this._offset + this._circularOffset;
+    const actualPosition =
+      this._position - this._alignPos - this._offset + this._circularOffset;
 
+    const pxToEm = (px: number) => px / 16;
     el.style[this._transform] = flicking.horizontal
-      ? `translate(${this._panelOrder === ORDER.RTL ? actualPosition : -actualPosition}px)`
+      ? `translate(${
+          this._panelOrder === ORDER.RTL
+            ? pxToEm(actualPosition)
+            : pxToEm(-actualPosition)
+        }em)`
       : `translate(0, ${-actualPosition}px)`;
 
     return this;
@@ -611,20 +687,26 @@ class Camera {
     const flicking = getFlickingAttached(this._flicking);
     const panels = flicking.renderer.panels;
 
-    const newVisiblePanels = panels.filter(panel => this.canSee(panel));
+    const newVisiblePanels = panels.filter((panel) => this.canSee(panel));
     const prevVisiblePanels = this._visiblePanels;
     this._visiblePanels = newVisiblePanels;
 
-    const added: Panel[] = newVisiblePanels.filter(panel => !includes(prevVisiblePanels, panel));
-    const removed: Panel[] = prevVisiblePanels.filter(panel => !includes(newVisiblePanels, panel));
+    const added: Panel[] = newVisiblePanels.filter(
+      (panel) => !includes(prevVisiblePanels, panel)
+    );
+    const removed: Panel[] = prevVisiblePanels.filter(
+      (panel) => !includes(newVisiblePanels, panel)
+    );
 
     if (added.length > 0 || removed.length > 0) {
       void flicking.renderer.render().then(() => {
-        flicking.trigger(new ComponentEvent(EVENTS.VISIBLE_CHANGE, {
-          added,
-          removed,
-          visiblePanels: newVisiblePanels
-        }));
+        flicking.trigger(
+          new ComponentEvent(EVENTS.VISIBLE_CHANGE, {
+            added,
+            removed,
+            visiblePanels: newVisiblePanels,
+          })
+        );
       });
     }
   }
@@ -639,11 +721,15 @@ class Camera {
 
     if (panels.length <= 0) {
       if (!needPanelTriggered.prev) {
-        flicking.trigger(new ComponentEvent(EVENTS.NEED_PANEL, { direction: DIRECTION.PREV }));
+        flicking.trigger(
+          new ComponentEvent(EVENTS.NEED_PANEL, { direction: DIRECTION.PREV })
+        );
         needPanelTriggered.prev = true;
       }
       if (!needPanelTriggered.next) {
-        flicking.trigger(new ComponentEvent(EVENTS.NEED_PANEL, { direction: DIRECTION.NEXT }));
+        flicking.trigger(
+          new ComponentEvent(EVENTS.NEED_PANEL, { direction: DIRECTION.NEXT })
+        );
         needPanelTriggered.next = true;
       }
 
@@ -664,8 +750,13 @@ class Camera {
     if (!needPanelTriggered.prev) {
       const firstPanelPrev = firstPanel.range.min;
 
-      if (cameraPrev <= (firstPanelPrev + needPanelThreshold) || cameraPosition <= (cameraRange.min + needPanelThreshold)) {
-        flicking.trigger(new ComponentEvent(EVENTS.NEED_PANEL, { direction: DIRECTION.PREV }));
+      if (
+        cameraPrev <= firstPanelPrev + needPanelThreshold ||
+        cameraPosition <= cameraRange.min + needPanelThreshold
+      ) {
+        flicking.trigger(
+          new ComponentEvent(EVENTS.NEED_PANEL, { direction: DIRECTION.PREV })
+        );
         needPanelTriggered.prev = true;
       }
     }
@@ -673,8 +764,13 @@ class Camera {
     if (!needPanelTriggered.next) {
       const lastPanelNext = lastPanel.range.max;
 
-      if (cameraNext >= (lastPanelNext - needPanelThreshold) || cameraPosition >= (cameraRange.max - needPanelThreshold)) {
-        flicking.trigger(new ComponentEvent(EVENTS.NEED_PANEL, { direction: DIRECTION.NEXT }));
+      if (
+        cameraNext >= lastPanelNext - needPanelThreshold ||
+        cameraPosition >= cameraRange.max - needPanelThreshold
+      ) {
+        flicking.trigger(
+          new ComponentEvent(EVENTS.NEED_PANEL, { direction: DIRECTION.NEXT })
+        );
         needPanelTriggered.next = true;
       }
     }
@@ -691,13 +787,21 @@ class Camera {
 
     const direction = newPos <= range.min ? DIRECTION.PREV : DIRECTION.NEXT;
 
-    flicking.trigger(new ComponentEvent(EVENTS.REACH_EDGE, {
-      direction
-    }));
+    flicking.trigger(
+      new ComponentEvent(EVENTS.REACH_EDGE, {
+        direction,
+      })
+    );
   }
 
   private _checkTranslateSupport = () => {
-    const transforms = ["webkitTransform", "msTransform", "MozTransform", "OTransform", "transform"];
+    const transforms = [
+      "webkitTransform",
+      "msTransform",
+      "MozTransform",
+      "OTransform",
+      "transform",
+    ];
 
     const supportedStyle = document.documentElement.style;
     let transformName = "";
@@ -708,7 +812,10 @@ class Camera {
     }
 
     if (!transformName) {
-      throw new FlickingError(ERROR.MESSAGE.TRANSFORM_NOT_SUPPORTED, ERROR.CODE.TRANSFORM_NOT_SUPPORTED);
+      throw new FlickingError(
+        ERROR.MESSAGE.TRANSFORM_NOT_SUPPORTED,
+        ERROR.CODE.TRANSFORM_NOT_SUPPORTED
+      );
     }
 
     this._transform = transformName;
@@ -726,9 +833,10 @@ class Camera {
       } else {
         const fallbackMode = flicking.circularFallback;
 
-        this._mode = fallbackMode === CIRCULAR_FALLBACK.BOUND
-          ? new BoundCameraMode(flicking)
-          : new LinearCameraMode(flicking);
+        this._mode =
+          fallbackMode === CIRCULAR_FALLBACK.BOUND
+            ? new BoundCameraMode(flicking)
+            : new LinearCameraMode(flicking);
       }
 
       this._circularEnabled = canSetCircularMode;
@@ -745,9 +853,9 @@ class Camera {
 
     const flicking = getFlickingAttached(this._flicking);
     const panels = flicking.renderer.panels;
-    const toggled = panels.map(panel => panel.toggle(prevPos, pos));
+    const toggled = panels.map((panel) => panel.toggle(prevPos, pos));
 
-    return toggled.some(isToggled => isToggled);
+    return toggled.some((isToggled) => isToggled);
   }
 }
 
